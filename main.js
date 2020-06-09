03cc33651adad2b3fc5ed6fe7214adf8e5a9ce0d
kengorian <kengorian556@gmail.com>
2020-06-09 22:12:11 +0900

    master

--- main.js
+++ main.js
@@ -4,4 +4,14 @@ var count=0;
 setInterval(function(){
     count++;
     document.getElementById("hello_text").textContent="初めのJavaScript("+count+")";
+    var td_array=document.getElementsByTagName("td");
+    var cells=[];
+    var index=0;
+    for(var row=0; row<20; row++){
+        cells[row]=[];
+        for(var col=0;col<10;col++){
+            cell[row][col]=td_array[index];
+            index++;
+        }
+    }
 },1000);
