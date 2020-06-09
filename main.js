8ded5ecf528dc69fc7797310ca741b4a5bcd53ae
kengorian <kengorian556@gmail.com>
2020-06-09 21:54:01 +0900

    master

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 document.getElementById("hello_text").textContent="はじめてのJavaScript";
+
+var count=0;
+setInterval(function(){
+    count++;
+    document.getElementById("hello_text").textContent="初めのJavaScript("+count+")";
+},1000);
